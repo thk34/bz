445284dfe129c3ffb348808f608421f38d112fb6
thk34 <103599660+thk34@users.noreply.github.com>
2023-02-16 00:04:53 +0100

    python bz.py
    python bz.py

---
+++ bz.py
@@ -0,0 +1,33 @@
+import time
+
+user = input("qui veut tu que je baise : ")
+
+def baise(user):
+    print("Je déshabille "+str(user))
+    time.sleep(6)
+    print("Je retourne "+str(user))
+    time.sleep(4)
+    print("J'écarte les fesses de "+str(user))
+    time.sleep(2)
+    print("Je met mon gros doigt et je le ressort du trou du cul de "+str(user))
+    time.sleep(3)
+    print("Je sniff mon doigt")
+    print("\x1B[3m ça pue... \x1B[0m")
+    time.sleep(4)
+    print("Je sors ma bite et je commence à exiter "+str(user))
+    time.sleep(5)
+    print("j'enfonce ma bite et j'accélere petit à petit dans le cul de "+str(user))
+    print(user + " jouie")
+    time.sleep(8)
+    print("J'éjacule dans le cul de "+str(user))
+    print("\x1B[3m #creampie \x1B[0m")
+    time.sleep(4)
+    print(user + " se retourne pour me sucer")
+    print(user + "\x1B[3m suce trop bien \x1B[0m")
+    time.sleep(4)
+    print(user + " s'assoit sur mon visage")
+    print("\x1B[3m mmmh \x1B[0m")
+       
+
+
+baise(user)
